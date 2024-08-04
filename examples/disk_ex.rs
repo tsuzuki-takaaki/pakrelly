85efc6b25134797dbeb02cb463101a4838fd75c3
tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-08-05 00:08:20 +0900

    fixup! Add DiskManger#read_page_data

--- examples/disk_ex.rs
+++ examples/disk_ex.rs
@@ -12,6 +12,7 @@ fn main() -> std::io::Result<()> {
     let mut buf = vec![0; PAGE_SIZE];
     // [CAUTION] Error: Error { kind: UnexpectedEof, message: "failed to fill whole buffer" }
     // -> 4096bytes読み込もうとしても、fileにそれだけのデータが存在しないと失敗する
+    // 書き込む際に、[0; PAGE_SIZE]にリサイズしてwriteすればいい
     disk_manager.read_page_data(page_id_bef, &mut buf)?;
     println!("{:?}", buf);
 

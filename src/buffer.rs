973105c0f4c85e36148f46f71237cc858f7abaa5
tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-08-17 14:55:17 +0900

    Add BufferPoolManager#fetch_page

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,13 +1,23 @@
+use std::io;
 use std::ops::{Index, IndexMut};
 use std::{
     cell::{Cell, RefCell},
     collections::HashMap,
     rc::Rc,
 };
+use thiserror;
 
 use crate::disk::{DiskManager, PageId, PAGE_SIZE};
 
 pub type Page = [u8; PAGE_SIZE];
+
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error("no free buffer available in buffer pool")]
+    NoFreeBuffer,
+}
 
 #[derive(Default, Clone, Copy)]
 pub struct BufferId(usize);
@@ -16,13 +26,14 @@ pub struct BufferId(usize);
 pub struct Buffer {
     pub page_id: PageId,
     pub page: RefCell<Page>,
-    pub is_dirty: Cell<bool>,
+    pub is_dirty: Cell<bool>, // BufferでPageに対してWriteした場合、is_dirtyをフラグとしてもつ(Diskに戻す時に整合性を取るため)
 }
 // Bufferに対するpointer + metadata(usage_countはそのbufferがどれだけ使われたかを持つ)
 // Rc: 対象のデータへの参照の数を実行時にtrackingする. カウントが0になってどこからも利用されてないことがわかったら対象のメモリ領域を自動で解放する
+//     Rc::cloneをすることによって、対象に対するReference counterがincrementされる
 pub struct Frame {
     usage_count: u64,
-    buffer: Rc<Buffer>,
+    buffer: Rc<Buffer>, // MySQLのような、一つのsessionに対して一つのThreadを割り当てるような設計を実現するためにはRcではなくて、Arcを使う必要が出てくる
 }
 pub struct BufferPool {
     buffers: Vec<Frame>,
@@ -42,6 +53,7 @@ impl IndexMut<BufferId> for BufferPool {
     }
 }
 
+// BufferPoolを管理する責務
 // どれだけのbufferをpoolingするかは、initialize(BufferPool::new)時に引数で渡す
 impl BufferPool {
     fn size(&self) -> usize {
@@ -83,9 +95,50 @@ impl BufferPool {
         BufferId((buffer_id.0 + 1) % self.size())
     }
 }
+
+// 実際のPageData供給layer(BufferPoolとDiskを両方みて、BufferPoolにあればそこから、なければDiskから取ってきてよしなに供給する責務)
 // poolを見に行って、あれば使い、なければDiskManager越しにDisk I/O
 pub struct BufferPoolManager {
     disk: DiskManager,
     pool: BufferPool,
     page_table: HashMap<PageId, BufferId>, // どのpageデータがどのbufferに入っているかをmapping
+}
+
+impl BufferPoolManager {
+    // Pageの実データを返す(Frame.buffer)
+    fn fetch_page(&mut self, page_id: PageId) -> Result<Rc<Buffer>, Error> {
+        // BufferPoolに必要なpageが存在する場合(HashMapで持っているpage_tableを探索)
+        if let Some(&buffer_id) = self.page_table.get(&page_id) {
+            let frame = &mut self.pool[buffer_id];
+            frame.usage_count += 1;
+            // Reference Counterをincrement
+            return Ok(Rc::clone(&frame.buffer));
+        }
+        // BufferPoolに存在せず、Diskからfetchする必要がある場合
+        // diskからfetchしたpageを乗せるbufferを探索する(必要無くなったBuffer) -> 存在しなければ早期return
+        let buffer_id = self.pool.evict().ok_or(Error::NoFreeBuffer)?;
+        // メモリキャッシュの必要がなくなったPageデータを管理しているFrame(捨てられる予定)
+        let frame = &mut self.pool[buffer_id];
+        // いらないbufferのpageデータのPageId
+        let evict_page_id = frame.buffer.page_id;
+        {
+            let buffer = Rc::get_mut(&mut frame.buffer).unwrap();
+            // cacheしてからPageに対してWriteしていた場合、Diskに戻す際にDisk Writeが必要になる
+            if buffer.is_dirty.get() {
+                self.disk
+                    .write_page_data(evict_page_id, buffer.page.get_mut())?;
+            }
+            // 捨てる対象のPageの整合性が取れたので、新しくBufferingするPageに関する処理に進む
+            buffer.page_id = page_id;
+            buffer.is_dirty.set(false);
+
+            self.disk.read_page_data(page_id, buffer.page.get_mut())?;
+            frame.usage_count = 1;
+        }
+
+        let page = Rc::clone(&frame.buffer);
+        self.page_table.remove(&evict_page_id);
+        self.page_table.insert(page_id, buffer_id);
+        Ok(page)
+    }
 }
